b933ef379a1b5597088e44b56364d16a5673ada4
Ayush Upadhyay <ayushupadhyay.tpa@jiostar.com>
2026-04-07 15:28:22 +0530

    remove InvitationModal component and its usage from the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import Editor from "@/features/editor";
-import { InvitationModal } from "@/components/invitation-modal";
 
 export default function Home() {
   return (
     <>
       <Editor />
-      <InvitationModal />
     </>
   );
 }
